93e8d9f7af3194e5ea5329d1986bf26c5467e54b
vannycoo1016 <167570910+vannycoo1016@users.noreply.github.com>
2024-05-01 01:42:00 -0400

    Character Controller & Shooting
    added in updated character controller and shooting mechanics. planning to add score feature + grave robbing in the morning :)

---
+++ GMAP345_Zombs/Assets/scripts/destroyOnContact.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyOnContact : MonoBehaviour
+
+/*
+* Author: Vanessa Cunha
+* Purpose: Destroys objects when shot
+* Class: GMAP 231
+* Assignment 6: NavMesh
+* Due Date: August 22nd, 2023
+*/
+
+{
+    //declare our list of strings we'll use if we want our projectile to destroy an object it hits
+    public List<string> destroyableObjects = new List<string>();
+    public GameObject explosionEffect;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //run through our list of tags we want to destroy
+        for (int i = 0; i < destroyableObjects.Count; i++)
+        {
+            //check if the hit object has one of our tags
+            if(collision.gameObject.CompareTag(destroyableObjects[i]))
+            {
+                //destroy the hit object and the projectile
+                Destroy(gameObject);
+                Destroy(collision.gameObject);
+                GameObject explosion = Instantiate(explosionEffect, transform.position, transform.rotation);
+                Destroy(explosion, 5);
+            }
+        }
+    }
+}
